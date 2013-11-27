740eb0211925cbf39524329160719f23c483b182
cloudera <cloudera@localhost.localdomain>
2013-11-27 22:22:15 +1100

    Initial commit of the boilerplate code

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+        @Test
+        public void shouldSayHello() {
+           assertTrue(true);
+        }
+}
